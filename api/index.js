e7b3a435461b4b58a9b0517690121c151d7cf280
Francesco ricci <francescosav.ricci@libero.it>
2024-03-06 15:49:44 +0100

    return challenge

--- api/index.js
+++ api/index.js
@@ -5,8 +5,12 @@ app.get("/", (req, res) => res.send("Youtube Automation Comment with HHTP webhoo
 
 app.get("/ytwebhook", (req, res) => 
 {
+    const challenge = req.query['hub.challenge']
     console.log(req.query);
     res.send("GET to HTTP webhook PubSubHubbub endpoint" + JSON.stringify(req.query));
+
+    if (challenge) return challenge;
+
 });
 
 
