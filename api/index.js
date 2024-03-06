9d511067dc14d901f0b1975abee4f7b69573020b
Francesco ricci <francescosav.ricci@libero.it>
2024-03-06 14:43:38 +0100

    add get and POST webhook rest API endpoint to PubSubHubbub

--- api/index.js
+++ api/index.js
@@ -3,6 +3,10 @@ const app = express();
 
 app.get("/", (req, res) => res.send("Youtube Automation Comment with HHTP webhook PubSubHubbub push Notification"));
 
+app.get("/ytwebhook", (req, res) => res.send("GET to webhook PubSubHubbub endpoint"));
+app.post("/ytwebhook", (req, res) => res.send("message post"));
+
+
 app.listen(3000, () => console.log("Server ready on port 3000."));
 
 module.exports = app;
